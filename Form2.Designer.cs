26ad3ddc6024058c24e0b5fb8cf1c592f4db2fad
atalayaygl <96258926+atalayaygl@users.noreply.github.com>
2024-03-06 18:58:41 +0300

    Add project files.

---
+++ Form2.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FoodName = new System.Windows.Forms.TextBox();
+            this.FullPrice = new System.Windows.Forms.TextBox();
+            this.HalfPrice = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.FoodCategory = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.save = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.update = new System.Windows.Forms.Button();
+            this.delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FoodName
+            // 
+            this.FoodName.Location = new System.Drawing.Point(145, 55);
+            this.FoodName.Margin = new System.Windows.Forms.Padding(4);
+            this.FoodName.Name = "FoodName";
+            this.FoodName.Size = new System.Drawing.Size(160, 22);
+            this.FoodName.TabIndex = 0;
+            this.FoodName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // FullPrice
+            // 
+            this.FullPrice.Location = new System.Drawing.Point(145, 157);
+            this.FullPrice.Margin = new System.Windows.Forms.Padding(4);
+            this.FullPrice.Name = "FullPrice";
+            this.FullPrice.Size = new System.Drawing.Size(160, 22);
+            this.FullPrice.TabIndex = 1;
+            this.FullPrice.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // HalfPrice
+            // 
+            this.HalfPrice.Location = new System.Drawing.Point(145, 109);
+            this.HalfPrice.Margin = new System.Windows.Forms.Padding(4);
+            this.HalfPrice.Name = "HalfPrice";
+            this.HalfPrice.Size = new System.Drawing.Size(160, 22);
+            this.HalfPrice.TabIndex = 2;
+            this.HalfPrice.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(50, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Yemeğin Adı:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 163);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Tam Fiyatı";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(50, 112);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Yarım Fiyatı";
+            // 
+            // FoodCategory
+            // 
+            this.FoodCategory.FormattingEnabled = true;
+            this.FoodCategory.Items.AddRange(new object[] {
+            "Et Yemekleri",
+            "Tavuk Çeşitleri",
+            "Köfte Çeşitleri",
+            "Sebze Yemekleri",
+            "Makarna Çeşitleri",
+            "Mezeler",
+            "Çorbalar",
+            "Tatlı",
+            "İçecek"});
+            this.FoodCategory.Location = new System.Drawing.Point(145, 215);
+            this.FoodCategory.Margin = new System.Windows.Forms.Padding(4);
+            this.FoodCategory.Name = "FoodCategory";
+            this.FoodCategory.Size = new System.Drawing.Size(160, 24);
+            this.FoodCategory.TabIndex = 6;
+            this.FoodCategory.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(50, 219);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Kategorisi";
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(240, 274);
+            this.save.Margin = new System.Windows.Forms.Padding(4);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(100, 28);
+            this.save.TabIndex = 8;
+            this.save.Text = "Kaydet";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.CausesValidation = false;
+            this.listBox1.ColumnWidth = 20;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(352, 58);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(495, 244);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // update
+            // 
+            this.update.Location = new System.Drawing.Point(133, 274);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(100, 28);
+            this.update.TabIndex = 12;
+            this.update.Text = "Duzenle";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(28, 274);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(99, 28);
+            this.delete.TabIndex = 13;
+            this.delete.Text = "Sil";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 363);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FoodCategory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HalfPrice);
+            this.Controls.Add(this.FullPrice);
+            this.Controls.Add(this.FoodName);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FoodName;
+        private System.Windows.Forms.TextBox FullPrice;
+        private System.Windows.Forms.TextBox HalfPrice;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox FoodCategory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button update;
+        private System.Windows.Forms.Button delete;
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPLACE;Integrated Security=True;Connection Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("insert into DbFood.dbo.FoodMenu values(@FoodName,@HalfPrice,@FullPrice,@FoodCategory)", con);
+            //try and catch
+            try
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@FoodName", FoodName.Text);
+                cmd.Parameters.AddWithValue("@HalfPrice", float.Parse(HalfPrice.Text));
+                cmd.Parameters.AddWithValue("@FullPrice", float.Parse(FullPrice.Text));
+                cmd.Parameters.AddWithValue("@FoodCategory", FoodCategory.SelectedItem);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Successfully Saved!");
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(" "+err);
+            }
+            RefreshListBox();
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            RefreshListBox();
+        }
+
+
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedItem = listBox1.SelectedItem.ToString();
+                string[] parts = selectedItem.Split(' ');
+
+                // Parçalanan değerleri ilgili TextBox'lara yerleştirin
+                if (parts.Length >= 4)
+                {
+                    FoodName.Text = parts[0];
+                    HalfPrice.Text = parts[1];
+                    FullPrice.Text = parts[2];
+                    FoodCategory.Text = parts[3];
+                }
+            }
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPLACE;Integrated Security=True;Connection Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("Update DbFood.dbo.FoodMenu set HalfPrice = @HalfPrice,FullPrice = @FullPrice,FoodCategory = @FoodCategory where FoodName = @FoodName", con);
+
+            try
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@HalfPrice", float.Parse(HalfPrice.Text));
+                cmd.Parameters.AddWithValue("@FullPrice", float.Parse(FullPrice.Text));
+                cmd.Parameters.AddWithValue("@FoodCategory", FoodCategory.Text);
+                cmd.Parameters.AddWithValue("@FoodName", FoodName.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Successfully Updated!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Yemeğin ismi değiştirilemez, silip yeniden ekleyin. Tamsayı değerler giriniz.");
+            }
+            RefreshListBox();
+
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=LAPLACE;Integrated Security=True;Connection Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("Delete DbFood.dbo.FoodMenu where FoodName = @Foodname", con);
+            try
+            {
+                con.Open();
+                cmd.Parameters.AddWithValue("@HalfPrice", float.Parse(HalfPrice.Text));
+                cmd.Parameters.AddWithValue("@FullPrice", float.Parse(FullPrice.Text));
+                cmd.Parameters.AddWithValue("@FoodCategory", FoodCategory.Text);
+                cmd.Parameters.AddWithValue("@FoodName", FoodName.Text);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Successfully Deleted!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Boyle bir yemek yok!");
+            }
+            RefreshListBox();
+        }
+        private void RefreshListBox()
+        {
+            // ListBox'ı temizle
+            listBox1.Items.Clear();
+
+            // Yeniden doldur
+            SqlConnection con = new SqlConnection("Data Source=LAPLACE;Integrated Security=True;Connection Timeout=30;Encrypt=True;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            SqlCommand cmd = new SqlCommand("select * from DbFood.dbo.FoodMenu", con);
+            try
+            {
+                con.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    string row = "";
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        // Her bir sütunu satır olarak birleştirin
+                        row += reader[i].ToString() + " ";
+                    }
+                    // Oluşturulan satırı ListBox'a ekleyin
+                    listBox1.Items.Add(row);
+                }
+                reader.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(" " + err);
+            }
+        }
+    }   
+ }
